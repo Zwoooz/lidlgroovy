e00aa2f465f11cbcd236fc97b84944707fb75e55
Zwoooz <benjamin@solbergarvidsson.se>
2025-05-28 01:58:39 +0200

    single file implementation of very simple play command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -4,8 +4,14 @@ import {
   MessageFlags,
   SlashCommandBuilder
 } from "discord.js";
-import { joinVoiceChannel } from "@discordjs/voice";
-import ytdl from "ytdl-core";
+import {
+  DiscordGatewayAdapterCreator,
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource
+} from "@discordjs/voice";
+import ytdl from "@distube/ytdl-core";
+import yts from 'yt-search';
 
 export default {
   data: new SlashCommandBuilder()
@@ -16,8 +22,6 @@ export default {
       .setRequired(true)),
 
   async execute(interaction: ChatInputCommandInteraction) {
-    // TODO: implement ytdl downloading from search string
-
     // TODO: maybe only register this command in servers and not DM if possible
     if (!interaction.inGuild()) {
       return interaction.reply({ content: 'This command can only be used in a server' });
@@ -29,6 +33,7 @@ export default {
         flags: MessageFlags.Ephemeral
       });
     }
+
     if (
       interaction.guild?.members.me?.voice.channelId &&
       (interaction.member as GuildMember).voice.channelId !==
@@ -40,5 +45,44 @@ export default {
       });
     }
 
+    // non-null assersion used as setRequired is true for the string option
+    // WARN: setRequired could be false in the future if integrating play/pause
+    const query: string = interaction.options.getString('query')!;
+
+    // replies to the command interaction so discord knows it's responding
+    interaction.reply({ content: `Searching YouTube for \`${query}\` ...` });
+
+    const results = await yts(query);
+    interaction.editReply(results.videos[0].url);
+
+
+    // voice connection and player 
+    // TODO: move into multiple files, with player events and such
+
+    // creates a voice connection in the voice channel the user currently is
+    const connection = joinVoiceChannel({
+      // non-null assersion used as this is checked above and will never be null
+      channelId: (interaction.member as GuildMember).voice.channelId!,
+      guildId: interaction.guildId,
+      adapterCreator: interaction.guild?.voiceAdapterCreator as DiscordGatewayAdapterCreator
+    });
+
+    // NOTE: this should happen somewhere else
+    // const connection = getVoiceConnection(interaction.guildId);
+
+    const player = createAudioPlayer();
+
+    // reads url and creates a readable stream that only contains audio
+    const stream = ytdl(results.videos[0].url, {
+      filter: 'audioonly',
+      dlChunkSize: 0
+    });
+
+    // creates a playable resource from the stream
+    const resource = createAudioResource(stream);
+
+    // attatches(subscribes) the connection to the player and plays the resource
+    connection.subscribe(player);
+    player.play(resource);
   }
 };
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,6 +5,6 @@ export default {
   once: true,
   execute(client: Client) {
     // non-null assertion used as this runs after the 'ready' event and will never be null
-    console.log(`${client.user!.username} logged in \n Ready`)
+    console.log(`${client.user!.username} logged in \n Ready`);
   }
-}
+};
