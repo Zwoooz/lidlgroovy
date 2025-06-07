b6205f186f7b427067096183644642e7aa348d6a
Zwoooz <benjamin@solbergarvidsson.se>
2025-06-07 22:51:28 +0200

    add clear command

---
+++ src/commands/music/clear.ts
@@ -0,0 +1,31 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { getPlayer } from "../../player/playerManager.js";
+import { AudioPlayerStatus } from "@discordjs/voice";
+import { getQueue, queueMap } from "../../player/queueManager.js";
+
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('clear')
+    .setDescription('Clears the queue and stops playback.'),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+
+    if (!interaction.inGuild()) {
+      return interaction.reply('This command can only be run in a server');
+    }
+
+    const player = await getPlayer(interaction.guildId, false);
+
+    if (!player || player.state.status === AudioPlayerStatus.Idle) {
+      return interaction.reply('Nothing is playing!');
+    }
+
+    const queue = getQueue(interaction.guildId);
+
+    await interaction.reply(`Cleared \`${queue.length}\` tracks from the queue!`);
+
+    queueMap.delete(interaction.guildId);
+    player.stop();
+  }
+};
