9b7ba8381f44c1d75fb98b8665e5cba16cdf93a5
Zwoooz <benjamin@solbergarvidsson.se>
2026-08-13 22:06:18 +0200

    timezone format option

--- src/commands/utility/timestamp.ts
+++ src/commands/utility/timestamp.ts
@@ -7,6 +7,21 @@ export default {
     .setDescription('Creates a discord timestamp')
     .addStringOption((option) =>
       option.setName('time').setDescription('time to create a timestamp from').setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName('format')
+        .setDescription('format of the timestamp')
+        .setChoices(
+          { name: 'time', value: 't' },
+          { name: 'exact time', value: 'T' },
+          { name: 'date', value: 'd' },
+          { name: 'full date', value: 'D' },
+          { name: 'date & time', value: 'f' },
+          { name: 'full date & time', value: 'F' },
+          { name: 'relative', value: 'R' },
+        )
+        .setRequired(false),
     ),
 
   async execute(interaction: ChatInputCommandInteraction) {
@@ -21,8 +36,12 @@ export default {
       );
     }
 
+    const format: string = interaction.options.getString('format', false) || 't';
+
     const unixSeconds = Math.floor(parsed.getTime() / 1000);
 
-    return await interaction.reply(`<t:${unixSeconds}:t>`);
+    return await interaction.reply(`<t:${unixSeconds}:${format}>`);
   },
 };
+
+// TODO: add per user saved timezones
